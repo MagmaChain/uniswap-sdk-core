fb4cabc1fa9b0ded2f9aac7d2bd83f00e5db755c
shahzeb8285 <shahzeb8285@gmail.com>
2025-04-20 04:59:23 +0530

    first commit

---
+++ src/chains.ts
@@ -0,0 +1,55 @@
+export enum ChainId {
+  MAINNET = 1,
+  GOERLI = 5,
+  MAGMA_TESTNET = 271,
+  SEPOLIA = 11155111,
+  OPTIMISM = 10,
+  OPTIMISM_GOERLI = 420,
+  OPTIMISM_SEPOLIA = 11155420,
+  ARBITRUM_ONE = 42161,
+  ARBITRUM_GOERLI = 421613,
+  ARBITRUM_SEPOLIA = 421614,
+  POLYGON = 137,
+  POLYGON_MUMBAI = 80001,
+  CELO = 42220,
+  CELO_ALFAJORES = 44787,
+  GNOSIS = 100,
+  MOONBEAM = 1284,
+  BNB = 56,
+  AVALANCHE = 43114,
+  BASE_GOERLI = 84531,
+  BASE = 8453
+}
+
+export const SUPPORTED_CHAINS = [
+  ChainId.MAINNET,
+  ChainId.OPTIMISM,
+  ChainId.MAGMA_TESTNET,
+
+  ChainId.OPTIMISM_GOERLI,
+  ChainId.ARBITRUM_ONE,
+  ChainId.ARBITRUM_GOERLI,
+  ChainId.POLYGON,
+  ChainId.POLYGON_MUMBAI,
+  ChainId.GOERLI,
+  ChainId.SEPOLIA,
+  ChainId.CELO_ALFAJORES,
+  ChainId.CELO,
+  ChainId.BNB,
+  ChainId.AVALANCHE,
+  ChainId.BASE,
+  ChainId.BASE_GOERLI
+] as const
+export type SupportedChainsType = typeof SUPPORTED_CHAINS[number]
+
+export enum NativeCurrencyName {
+  // Strings match input for CLI
+  ETHER = 'ETH',
+  MAGMA = 'LAVA',
+  MATIC = 'MATIC',
+  CELO = 'CELO',
+  GNOSIS = 'XDAI',
+  MOONBEAM = 'GLMR',
+  BNB = 'BNB',
+  AVAX = 'AVAX'
+}
